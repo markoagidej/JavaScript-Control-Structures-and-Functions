ff63aee746155536889a873437c9c25fc801b761
Marko <markogidej@yahoo.com>
2024-06-19 17:45:50 -0400

    Q2 all tasks done

--- banking.js
+++ banking.js
@@ -1,7 +1,20 @@
-// Task 1: Create a function to handle deposits into a bank account.
+let balance
 
+// Task 1: Create a function to handle deposits into a bank account.
+function deposit(amount) {
+    balance += amount
+}
 
 // Task 2: Implement a function to handle withdrawals from a bank account, ensuring sufficient balance.
-
+function withdraw(amount) {
+    if (balance - amount > 0) {
+        balance -= amount
+    } else {
+        console.log(`Insufficient funds to withdraw ${amount}`)
+    }
+}
 
 // Task 3: Develop a function to check the current balance of the account.
+function check() {
+    console.log(`Current balance: ${balance}`)
+}
