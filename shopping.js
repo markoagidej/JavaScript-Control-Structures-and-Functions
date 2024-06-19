eba0ba8ffa945da9e463a6de1dd010f27e4449b8
Marko <markogidej@yahoo.com>
2024-06-19 17:41:23 -0400

    Q1 alls taks done

--- shopping.js
+++ shopping.js
@@ -1,9 +1,21 @@
-let isLoggedIn = True
+let isLoggedIn = true
+let items = {apple:2, orange:3, banana:1}
+let cart = ["apple", "apple", "orange"]
 
 // Task 1: Implement a conditional statement to check if a user is logged in before allowing them to add items to the cart.
+if (isLoggedIn) {
+    // Task 2: Use a loop to display the list of available products to the user and add functionality for users to add products to their cart.
+    /*
+    Unsure of how to implement user functionality at this point
+    */
+    for (const item in items) {
+        console.log(item)
+    }
 
-
-// Task 2: Use a loop to display the list of available products to the user and add functionality for users to add products to their cart.
-
-
-// Task 3: Calculate the total cost of the items in the cart and display it to the user.
+    // Task 3: Calculate the total cost of the items in the cart and display it to the user.
+    let totalCost = 0
+    for (const item in cart) {
+        totalCost += items[cart[item]]
+    }
+    console.log(totalCost)
+}
