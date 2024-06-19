25c5e977aa72ab046bc0ac643346fa5aeefff0aa
Marko <markogidej@yahoo.com>
2024-06-19 17:27:34 -0400

    Init Commit

---
+++ banking.js
@@ -0,0 +1,7 @@
+// Task 1: Create a function to handle deposits into a bank account.
+
+
+// Task 2: Implement a function to handle withdrawals from a bank account, ensuring sufficient balance.
+
+
+// Task 3: Develop a function to check the current balance of the account.
---
+++ shopping.js
@@ -0,0 +1,9 @@
+let isLoggedIn = True
+
+// Task 1: Implement a conditional statement to check if a user is logged in before allowing them to add items to the cart.
+
+
+// Task 2: Use a loop to display the list of available products to the user and add functionality for users to add products to their cart.
+
+
+// Task 3: Calculate the total cost of the items in the cart and display it to the user.
