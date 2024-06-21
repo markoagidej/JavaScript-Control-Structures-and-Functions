6a0a858edf12edd5ef2e59afe5b6cf3da3f12f23
Marko <markogidej@yahoo.com>
2024-06-21 10:54:18 -0400

    Added adding to cart

--- shopping.js
+++ shopping.js
@@ -1,13 +1,20 @@
 let isLoggedIn = true
 let items = {apple:2, orange:3, banana:1}
-let cart = ["apple", "apple", "orange"]
+let cart = []
+
+function addItem(cart, item) {
+    cart.push(item)
+    return cart
+}
 
 // Task 1: Implement a conditional statement to check if a user is logged in before allowing them to add items to the cart.
 if (isLoggedIn) {
     // Task 2: Use a loop to display the list of available products to the user and add functionality for users to add products to their cart.
-    /*
-    Unsure of how to implement user functionality at this point
-    */
+    
+    addItem(cart, "apple");
+    addItem(cart, "apple");
+    addItem(cart, "orange");
+
     for (const item in items) {
         console.log(item)
     }
